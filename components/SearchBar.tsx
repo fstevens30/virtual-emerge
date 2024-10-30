774f4c0b2adc997e1a470781be69345c8b7bac66
Flynn Stevens <flynnstevens30@gmail.com>
2024-10-30 13:13:41 +1300

    Fix search bar text color"

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -38,7 +38,7 @@ export const SearchBar = ({ defaultValue }: iDefault) => {
         value={inputValue ?? ''}
         onChange={handleChange}
         onKeyDown={handleKeyPress}
-        className='bg-white text-white'
+        className='bg-white text-black'
       />
       <Button className='ml-2' onClick={handleSearch}>
         <Search size={48} />
