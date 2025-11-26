be200269339c24b0b237e5a82201990851b98276
Flynn Stevens <flynnstevens30@gmail.com>
2025-11-27 09:08:31 +1300

    Emerge 2025 S2 date set

--- config/site.ts
+++ config/site.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   name: 'Virtual Emerge',
   description:
     'Explore Ara Institute of Canterbury Bachelor of Information and Communication Technologies student final year projects, in the Virtual Emerge showcase.',
-  nextEvent: new Date('2024-12-04')
+  nextEvent: new Date('2025-12-02')
 }
